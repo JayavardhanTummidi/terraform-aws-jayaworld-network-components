14a1f288fcb8d9fc83d9b2c002fbadb9ffb12fb2
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-10-22 00:00:23 -0400

    network

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "jayaworld-aws-vpc" {
 
 # Create Subnets
 resource "aws_subnet" "jayaworld-aws-subnets" {
-  count                           = length(var.subnet_cidr_blocks) > 0 && (length(var.subnet_cidr_blocks) >= length (var.subnet_azs)) ? length(var.subnet_cidr_blocks) : 0
+  count                           = length(var.subnet_cidr_blocks) > 0 && (length(var.subnet_cidr_blocks) >= length(var.subnet_azs)) ? length(var.subnet_cidr_blocks) : 0
   vpc_id                          = var.vpc_id
   cidr_block                      = element(concat(var.subnet_cidr_blocks, [""]), count.index)
   availability_zone               = element(concat(var.subnet_azs, [""]), count.index)
@@ -26,4 +26,36 @@ resource "aws_subnet" "jayaworld-aws-subnets" {
   customer_owned_ipv4_pool        = var.customer_ip_address
   outpost_arn                     = var.customer_outpost_arn
   tags                            = merge(var.subnet_tags)
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "jayaworld-aws-igw" {
+  vpc_id = var.vpc_id
+  tags   = merge(var.igw_tags)
+}
+
+# Create Route Table
+resource "aws_route_table" "jayaworld-aws-rt" {
+  vpc_id = var.vpc_id
+  route  = var.route
+  tags   = merge(var.rt_tags)
+}
+
+# Edit Routes in the route table
+resource "aws_route" "jayaworld-aws-route" {
+  route_table_id = var.route_table_id
+  # Route Destinations
+  destination_cidr_block      = var.route_destination_cidr_block
+  destination_ipv6_cidr_block = var.route_destination_ipv6_cidr_block
+  # Route Targets
+  carrier_gateway_id        = var.route_carrier_gateway_id
+  egress_only_gateway_id    = var.route_egress_only_gateway_id
+  gateway_id                = var.route_gateway_id
+  instance_id               = var.route_instance_id
+  nat_gateway_id            = var.route_nat_gateway_id
+  local_gateway_id          = var.route_local_gateway_id
+  network_interface_id      = var.route_network_interface_id
+  transit_gateway_id        = var.route_transit_gateway_id
+  vpc_endpoint_id           = var.route_vpc_endpoint_id
+  vpc_peering_connection_id = var.route_vpc_peering_connection_id
 }
--- variables.tf
+++ variables.tf
@@ -46,12 +46,6 @@ variable "vpc_tags" {
   default     = {}
 }
 
-variable "create_aws_subnet" {
-  description = "Do you want to create a new subnet ? default is false"
-  type        = bool
-  default     = false
-}
-
 variable "vpc_id" {
   description = "The VPC ID."
   type        = string
@@ -111,3 +105,104 @@ variable "subnet_tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "route" {
+  description = "A list of route objects. Their keys are documented below. This argument is processed in attribute-as-blocks mode. This means that omitting this argument is interpreted as ignoring any existing routes. To remove all managed routes an empty list should be specified"
+  type        = list(any)
+  default     = []
+}
+
+variable "igw_tags" {
+  description = "internet gateway tags"
+  type        = map(string)
+  default = {
+    Name = "Jayaworld-igw"
+  }
+}
+
+variable "rt_tags" {
+  description = "route table tags"
+  type        = map(string)
+  default = {
+    Name = "Jayaworld-rt"
+  }
+}
+
+variable "route_table_id" {
+  description = "The ID of the routing table"
+  type        = string
+  default     = ""
+}
+
+variable "route_destination_cidr_block" {
+  description = "The destination CIDR block."
+  type        = string
+  default     = ""
+}
+
+variable "route_destination_ipv6_cidr_block" {
+  description = "The destination IPv6 CIDR block."
+  type        = string
+  default     = null
+}
+
+variable "route_carrier_gateway_id" {
+  description = "Identifier of a carrier gateway. This attribute can only be used when the VPC contains a subnet which is associated with a Wavelength Zone."
+  type        = string
+  default     = ""
+}
+
+variable "route_egress_only_gateway_id" {
+  description = " Identifier of a VPC Egress Only Internet Gateway."
+  type        = string
+  default     = ""
+}
+
+variable "route_gateway_id" {
+  description = "Identifier of a VPC internet gateway or a virtual private gateway."
+  type        = string
+  default     = ""
+}
+
+variable "route_instance_id" {
+  description = "Identifier of an EC2 instance."
+  type        = string
+  default     = ""
+}
+
+variable "route_nat_gateway_id" {
+  description = "Identifier of a VPC NAT gateway."
+  type        = string
+  default     = ""
+}
+
+variable "route_local_gateway_id" {
+  description = "Identifier of a Outpost local gateway."
+  type        = string
+  default     = ""
+}
+
+variable "route_network_interface_id" {
+  description = "Identifier of an EC2 network interface."
+  type        = string
+  default     = ""
+}
+
+variable "route_transit_gateway_id" {
+  description = "Identifier of an EC2 Transit Gateway."
+  type        = string
+  default     = ""
+}
+
+variable "route_vpc_endpoint_id" {
+  description = "Identifier of a VPC Endpoint."
+  type        = string
+  default     = ""
+}
+
+variable "route_vpc_peering_connection_id" {
+  description = "Identifier of a VPC peering connection."
+  type        = string
+  default     = ""
+}
+
