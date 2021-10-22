57f525121b1081f0c6ba9d708b19d167edb3fe8d
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-10-22 00:13:14 -0400

    networking

--- main.tf
+++ main.tf
@@ -50,14 +50,14 @@ resource "aws_route" "jayaworld-aws-route" {
   destination_cidr_block      = var.route_destination_cidr_block
   destination_ipv6_cidr_block = var.route_destination_ipv6_cidr_block
   # Route Targets
-  carrier_gateway_id        = var.route_carrier_gateway_id
-  egress_only_gateway_id    = var.route_egress_only_gateway_id
-  gateway_id                = var.route_gateway_id
-  instance_id               = var.route_instance_id
-  nat_gateway_id            = var.route_nat_gateway_id
-  local_gateway_id          = var.route_local_gateway_id
-  network_interface_id      = var.route_network_interface_id
-  transit_gateway_id        = var.route_transit_gateway_id
-  vpc_endpoint_id           = var.route_vpc_endpoint_id
-  vpc_peering_connection_id = var.route_vpc_peering_connection_id
+  #carrier_gateway_id        = var.route_carrier_gateway_id
+  #egress_only_gateway_id    = var.route_egress_only_gateway_id
+  #gateway_id                = var.route_gateway_id
+  #instance_id               = var.route_instance_id
+  #nat_gateway_id            = var.route_nat_gateway_id
+  local_gateway_id = var.route_local_gateway_id
+  #network_interface_id      = var.route_network_interface_id
+  #transit_gateway_id        = var.route_transit_gateway_id
+  #vpc_endpoint_id           = var.route_vpc_endpoint_id
+  #vpc_peering_connection_id = var.route_vpc_peering_connection_id
 }
